4dd787be63d3a2f7448ee52f85ddccfdad903c02
Sahil Bhatia <sahil@joshsoftware.com>
2017-02-18 00:37:54 +0530

    very simple task list (first iteration)

--- src/App.js
+++ src/App.js
@@ -1,29 +1,122 @@
 import React, { PureComponent } from 'react';
-import { Well } from 'react-bootstrap';
 
-import Loader from './Loader.js';
 import './App.css';
 
 class App extends PureComponent {
-  render() {
-    return (
-      <Well>
-        <Well>
-          <label onClick={this.clicked}>Well 1</label>
-        </Well>
-        <Well>
-          <label onClick={this.clicked}>Well 2</label>
-        </Well>
-        <Well className='relative'>
-          <label onClick={this.clicked}>Well 3</label>
-          <Loader />
-        </Well>
-      </Well>
-    )
+  constructor(props) {
+    super(props);
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.addTask = this.addTask.bind(this);
+    this.toggleStrikeClass = this.toggleStrikeClass.bind(this);
   }
 
-  clicked(event) {
-    console.log(`${event.target.textContent} called!`);
+  propTypes: {
+    placeholderText: React.PropTypes.String
+  }
+
+  state = {
+    inputValue: '',
+    placeholderText: this.props.placeholderText || 'Default placeholder...',
+    tasks: [],
+    preventSubmission: true,
+    errorMsg: null
+  }
+
+  handleInputChange(evt) {
+    let inputText = evt.target.value;
+
+    if ( inputText.trim() !== '' ) {
+      this.setState({
+        errorMsg: null,
+        preventSubmission: false,
+        inputValue: inputText
+      });
+    } else {
+      this.setState({
+        errorMsg: 'Must enter a value!',
+        preventSubmission: true,
+        inputValue: inputText
+      });
+    }
+  }
+
+  addTask(evt) {
+    let newTask;
+
+    evt.preventDefault();
+
+    newTask = this.state.inputValue;
+
+    if ( this.state.tasks.indexOf(newTask) === -1 ) {
+      this.setState({
+        tasks: this.state.tasks.concat(this.state.inputValue),
+        inputValue: '',
+        preventSubmission: true
+      });
+    } else {
+      this.setState({
+        errorMsg: `${newTask} already exists in the list!`,
+        inputValue: '',
+        preventSubmission: true
+      });
+    }
+  }
+
+  toggleStrikeClass(evt) {
+    evt.target.classList.toggle('strike');
+  }
+
+  removeTask(taskIndex) {
+    let newTasks;
+
+    newTasks = this.state.tasks.filter((item, index) => {
+      return index !== taskIndex;
+    });
+
+    this.setState({
+      tasks: newTasks
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={ this.addTask }>
+          <input
+            type="text"
+            placeholder={ this.state.placeholderText }
+            value={ this.state.inputValue }
+            onChange={ this.handleInputChange }
+          />
+          <button type='submit' disabled={this.state.preventSubmission}>
+            { this.state.preventSubmission }
+            Add Task
+          </button>
+        </form>
+
+        <div> { this.state.errorMsg } </div>
+
+        <ul>
+          { this.state.tasks.map((task, index) => {
+            return (
+              <li key={ index }>
+                <span onClick={ this.toggleStrikeClass }>
+                  { task }
+                </span>
+
+                <span
+                  className='removeItem'
+                  onClick={ () => { this.removeTask(index) } }
+                >
+                  remove
+                </span>
+              </li>
+            )
+          }) }
+        </ul>
+      </div>
+    )
   }
 }
 
